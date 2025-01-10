c4ae2d69c7adeb5751ad17b97086b47fc0f9f537
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-01-09 21:34:42 -0300

    Criação da classe Professor: - Implementação dos atributos nome, idade e tempo de trabalho para representar um professor. - Métodos getters e setters para acesso e modificação dos atributos. - Implementação do método toString para uma representação clara do objeto.

---
+++ src/main/java/com/projeto/entidades/Professor.java
@@ -0,0 +1,49 @@
+package main.java.com.projeto.entidades;
+
+public class Professor {
+    private String nome;
+    private int idade;
+    private int tempoTrabalho;
+
+    public Professor() {
+    }
+
+    public Professor(String nome, int idade, int tempoTrabalho) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tempoTrabalho = tempoTrabalho;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getTempoTrabalho() {
+        return tempoTrabalho;
+    }
+
+    public void setTempoTrabalho(int tempoTrabalho) {
+        this.tempoTrabalho = tempoTrabalho;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", tempoTrabalho=" + tempoTrabalho +
+                '}';
+    }
+}
