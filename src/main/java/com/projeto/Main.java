1211e23c6f9e7866798343e497fe237e0302bc87
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-01-16 22:42:57 -0300

    Adiciona lógica de gerenciamento de usuários e funcionalidades para Professores, Diretores e Alunos
    - Implementa o menu principal com opções para Funcionário (Professor ou Diretor) ou Aluno.
    - Permite criar ou entrar com usuário existente para Professores, Diretores e Alunos.
    - Professores podem adicionar/remover alunos, além de listar alunos associados.
    - Diretores podem promover/remover professores, adicionar/remover alunos e listar todos os usuários.
    - Alunos podem adicionar/remover cursos e ativar/desativar conta.
    - Utilização de listas para armazenar objetos de alunos, professores e diretores.
    - Adiciona tratamento de exceções para entradas inválidas e validações de dados.

--- src/main/java/com/projeto/Main.java
+++ src/main/java/com/projeto/Main.java
@@ -1,9 +1,6 @@
 package main.java.com.projeto;
 
-import main.java.com.projeto.entidades.Aluno;
-import main.java.com.projeto.entidades.Curso;
-import main.java.com.projeto.entidades.Diretor;
-import main.java.com.projeto.entidades.Professor;
+import main.java.com.projeto.entidades.*;
 
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -30,7 +27,8 @@ public class Main {
 
                 switch (opcaoCargo) {
                     case 1 -> {
-                        while (true) {
+                        boolean continuarProfessorDiretor = true;
+                        while (continuarProfessorDiretor) {
                             System.out.println("Você é:\n" +
                                     "1 - Professor\n" +
                                     "2 - Diretor");
@@ -40,20 +38,173 @@ public class Main {
 
                             if (opcaoFuncionario == 1) {
                                 System.out.println("Opção escolhida: Professor");
-                                criarOuEntrarUsuario("Professor", listaProfessores, scan);
-                                break;
+                                Professor professor = criarOuEntrarUsuario("Professor", listaProfessores, scan);
+
+                                if (professor != null) {
+                                    boolean continuarProfessor = true;
+                                    while (continuarProfessor) {
+                                        System.out.println("O que deseja realizar:\n" +
+                                                "1 - Listar alunos\n" +
+                                                "2 - Adicionar aluno\n" +
+                                                "3 - remover aluno\n" +
+                                                "4 - sair");
+
+                                        int opcaoProfessor = scan.nextInt();
+                                        scan.nextLine();
+
+                                        switch (opcaoProfessor) {
+                                            case 1:
+                                                professor.listarAlunos();
+                                                break;
+                                            case 2:
+                                                System.out.println("Digite o nome do aluno que deseja adicionar:");
+                                                String nomeAlunoAdicionar = scan.nextLine().toLowerCase();
+                                                professor.adicionarAluno(nomeAlunoAdicionar);
+                                                break;
+                                            case 3:
+                                                System.out.println("Digite o nome do aluno que deseja remover:");
+                                                String nomeAlunoRemover = scan.nextLine().toLowerCase();
+                                                professor.removerAluno(nomeAlunoRemover);
+                                                break;
+                                            case 4:
+                                                System.out.println("Voltando ao menu principal...");
+                                                continuarProfessor = false;
+                                                continuarProfessorDiretor = false;
+                                                break;
+                                            default:
+                                                System.out.println("Opção inválida, tente novamente.");
+                                        }
+                                    }
+                                }
                             } else if (opcaoFuncionario == 2) {
                                 System.out.println("Opção escolhida: Diretor");
-                                criarOuEntrarUsuario("Diretor", listaDiretores, scan);
-                                break;
-                            } else {
-                                System.out.println("Opção inválida, tente novamente.");
+                                Diretor diretor = criarOuEntrarUsuario("Diretor", listaDiretores, scan);
+
+                                if (diretor != null) {
+                                    boolean continuarDiretor = true;
+                                    while (continuarDiretor) {
+                                        System.out.println("O que deseja realizar:\n" +
+                                                "1 - Promover professor\n" +
+                                                "2 - Adicionar professor\n" +
+                                                "3 - Remover professor\n" +
+                                                "4 - Adicionar aluno\n" +
+                                                "5 - Remover aluno\n" +
+                                                "6 - Listar alunos e professores\n" +
+                                                "7 - Sair");
+
+                                        int opcaoDiretor = scan.nextInt();
+                                        scan.nextLine();
+
+                                        switch (opcaoDiretor) {
+                                            case 1:
+                                                System.out.println("Digite o nome do professor que deseja promover:");
+                                                String nomeProfessorPromovido = scan.nextLine().toLowerCase();
+                                                diretor.promover(nomeProfessorPromovido);
+                                                break;
+                                            case 2:
+                                                System.out.println("Digite o nome do Professor que deseja adicionar:");
+                                                String nomeProfessorAdicionar = scan.nextLine().toLowerCase();
+                                                diretor.adicionarProfessor(nomeProfessorAdicionar);
+                                                break;
+                                            case 3:
+                                                System.out.println("Digite o nome do Professor que deseja remover:");
+                                                String nomeProfessorRemover = scan.nextLine().toLowerCase();
+                                                diretor.removerProfessor(nomeProfessorRemover);
+                                                break;
+                                            case 4:
+                                                System.out.println("Digite o nome do aluno que deseja adicionar:");
+                                                String nomeAlunoAdicionar = scan.nextLine().toLowerCase();
+                                                diretor.adicionarAluno(nomeAlunoAdicionar);
+                                                break;
+                                            case 5:
+                                                System.out.println("Digite o nome do aluno que deseja remover:");
+                                                String nomeAlunoRemover = scan.nextLine().toLowerCase();
+                                                diretor.removerAluno(nomeAlunoRemover);
+                                                break;
+                                            case 6:
+                                                diretor.listarAlunosProfessores();
+                                                break;
+                                            case 7:
+                                                System.out.println("Voltando ao menu principal...");
+                                                continuarDiretor = false;
+                                                break;
+                                            default:
+                                                System.out.println("Opção inválida, tente novamente.");
+                                        }
+                                    }
+                                } else {
+                                    System.out.println("Erro ao tentar encontrar ou criar o diretor. Tente novamente.");
+                                }
                             }
                         }
                     }
                     case 2 -> {
                         System.out.println("Opção escolhida: Aluno");
-                        criarOuEntrarUsuario("Aluno", listaAlunos, scan);
+                        Aluno aluno = criarOuEntrarUsuario("Aluno", listaAlunos, scan);
+
+                        if (aluno != null) {
+                            boolean continuarAluno = true;
+                            while (continuarAluno) {
+                                if (aluno.getContaAtiva()) {
+                                    System.out.println("O que deseja realizar:\n" +
+                                            "1 - Listar cursos\n" +
+                                            "2 - Adicionar curso\n" +
+                                            "3 - remover curso\n" +
+                                            "4 - trancar conta\n" +
+                                            "5 - sair");
+
+                                    int opcaoAluno = scan.nextInt();
+                                    scan.nextLine();
+
+                                    switch (opcaoAluno) {
+                                        case 1:
+                                            aluno.listarCursos();
+                                            break;
+                                        case 2:
+                                            System.out.println("Digite o nome do curso que deseja adicionar:");
+                                            String nomeCursoAdicionar = scan.nextLine().toLowerCase();
+                                            aluno.adicionarCurso(nomeCursoAdicionar);
+                                            break;
+
+                                        case 3:
+                                            System.out.println("Digite o nome do curso que deseja remover:");
+                                            String nomeCursoRemover = scan.nextLine().toLowerCase();
+                                            aluno.removerCurso(nomeCursoRemover);
+                                            break;
+
+                                        case 4:
+                                            System.out.println("Desativando sua conta");
+                                            aluno.setContaAtiva(false);
+                                            break;
+
+                                        case 5:
+                                            System.out.println("Voltando ao menu principal...");
+                                            continuarAluno = false;
+                                            break;
+
+                                        default:
+                                            System.out.println("Opção inválida, tente novamente.");
+                                    }
+                                } else {
+                                    System.out.println("O que deseja realizar:\n" +
+                                            "1 - Ativar contaa\n" +
+                                            "2 - Sair");
+
+                                    int opcaoAtivarConta = scan.nextInt();
+                                    scan.nextLine();
+
+                                    if (opcaoAtivarConta == 1) {
+                                        System.out.println("Ativando sua conta");
+                                        aluno.setContaAtiva(true);
+                                    } else if (opcaoAtivarConta == 2) {
+                                        System.out.println("Voltando ao menu principal...");
+                                        continuarAluno = false;
+                                    } else {
+                                        System.out.println("Opção inválida! Por favor, digite um número válido.");
+                                    }
+                                }
+                            }
+                        }
                     }
                     case 3 -> {
                         System.out.println("Encerrando...");
@@ -69,7 +220,7 @@ public class Main {
         scan.close();
     }
 
-    private static <T> void criarOuEntrarUsuario(String cargo, List<T> lista, Scanner scan) {
+    private static <T> T criarOuEntrarUsuario(String cargo, List<T> lista, Scanner scan) {
         System.out.println("O que deseja:\n" +
                 "1 - Entrar como usuário existente\n" +
                 "2 - Criar usuário");
@@ -80,19 +231,26 @@ public class Main {
 
             if (opcao == 1) {
                 System.out.println("Digite o nome do seu usuário:");
-                String nome = scan.nextLine();
-                boolean usuarioEncontrado = lista.stream().anyMatch(usuario -> {
-                    if (usuario instanceof Aluno aluno) return aluno.getNome().equals(nome);
-                    if (usuario instanceof Professor professor) return professor.getNome().equals(nome);
-                    if (usuario instanceof Diretor diretor) return diretor.getNome().equals(nome);
-                    return false;
-                });
+                String nome = scan.nextLine().toLowerCase();
 
-                if (usuarioEncontrado) {
-                    System.out.println("Usuário encontrado! Entrando como: " + nome);
-                } else {
-                    System.out.println("Usuário não encontrado!");
+                for (T usuario : lista) {
+                    if (usuario instanceof Aluno aluno && aluno.getNome().toLowerCase().equals(nome)) {
+                        System.out.println("Usuário encontrado! Entrando como: " + nome);
+                        return (T) aluno;
+                    }
+
+                    if (usuario instanceof Professor professor && professor.getNome().toLowerCase().equals(nome)) {
+                        System.out.println("Usuário encontrado! Entrando como: " + nome);
+                        return (T) professor;
+                    }
+
+
+                    if (usuario instanceof Diretor diretor && diretor.getNome().toLowerCase().equals(nome)) {
+                        System.out.println("Usuário encontrado! Entrando como: " + nome);
+                    }
                 }
+                System.out.println("Usuário não encontrado!");
+                return null;
 
             } else if (opcao == 2) {
                 System.out.println("Digite seu nome:");
@@ -102,31 +260,54 @@ public class Main {
                     case "Aluno" -> {
                         System.out.println("Digite o nome do curso em que frequenta:");
                         String nomeCurso = scan.nextLine();
-                        Aluno aluno = new Aluno(nome, List.of(new Curso(nomeCurso, null)));
+                        Aluno aluno = new Aluno(nome, new ArrayList<>(List.of(new Curso(nomeCurso, null))));
                         lista.add((T) aluno);
+                        aluno.setContaAtiva(true);
                         System.out.println("Aluno " + nome + " criado com sucesso!");
+                        return (T) aluno;
                     }
                     case "Professor" -> {
                         System.out.println("Digite sua idade:");
                         int idade = scan.nextInt();
-                        System.out.println("Digite seu tempo de trabalho (anos):");
-                        int tempoDeTrabalho = scan.nextInt();
-                        scan.nextLine();
+                        if (idade < 18) {
+                            System.out.println("Idade inválida. Precisa ser maior que 18");
+                        } else {
+                            System.out.println("Digite seu tempo de trabalho (anos):");
+                            int tempoDeTrabalho = scan.nextInt();
+                            scan.nextLine();
+                            if (tempoDeTrabalho < 0) {
+                                System.out.println("Tempo de trabalho inválido. Precisa ser maior que 0");
+                            } else {
 
-                        Professor professor = new Professor(nome, idade, tempoDeTrabalho);
-                        lista.add((T) professor);
-                        System.out.println("Professor " + nome + " criado com sucesso!");
+                                Professor professor = new Professor(nome, idade, tempoDeTrabalho);
+                                Turma turma = new Turma();
+
+                                professor.setTurma(turma);
+                                lista.add((T) professor);
+                                System.out.println("Professor " + nome + " criado com sucesso!");
+                                return (T) professor;
+                            }
+                        }
                     }
                     case "Diretor" -> {
-                        System.out.println("Digite seu tempo de cargo (anos):");
+                        System.out.println("Digite seu tempo de trabalho (anos):");
                         int tempoDeTrabalho = scan.nextInt();
-                        System.out.println("Digite seu salário:");
-                        double salario = scan.nextDouble();
-                        scan.nextLine();
+                        if (tempoDeTrabalho < 0) {
+                            System.out.println("Tempo de trabalho inválido. Precisa ser maior que 0");
+                        } else {
+                            System.out.println("Digite seu salário:");
+                            double salario = scan.nextDouble();
+                            scan.nextLine();
+                            if (salario < 0) {
+                                System.out.println("Salário inválido Precisa ser maior que 0");
+                            } else {
 
-                        Diretor diretor = new Diretor(nome, tempoDeTrabalho, salario);
-                        lista.add((T) diretor);
-                        System.out.println("Diretor " + nome + " criado com sucesso!");
+                                Diretor diretor = new Diretor(nome, tempoDeTrabalho, salario);
+                                lista.add((T) diretor);
+                                System.out.println("Diretor " + nome + " criado com sucesso!");
+                                return (T) diretor;
+                            }
+                        }
                     }
                 }
             } else {
@@ -136,5 +317,6 @@ public class Main {
             System.out.println("Erro ao criar ou entrar como usuário: " + "Por favor, digite um número válido");
             scan.nextLine();
         }
+        return null;
     }
 }
